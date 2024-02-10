163e7a347053ecc3744a61eaefe502f2364087a8
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2024-02-10 20:38:53 +0530

    css changes

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -2,6 +2,31 @@ const ScoreBoard = ({ score }) => {
     return (
         <div className="scoreboard">
         <h2>Score: {score}</h2>
+        <style>
+            {`
+            .scoreboard {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100px;
+                color: #FF0000;
+                font-size: 3rem;
+                margin: 10px;
+                margin-top: 20px;
+                font-family: 'Kalam', cursive;
+            }
+            @media (max-width: 768px) {
+                .scoreboard {
+                    font-size: 2.5rem;
+                }
+            }
+            @media (max-width: 480px) {
+                .scoreboard {
+                    font-size: 2rem;
+                }
+            }
+            `}
+        </style>
         </div>
     );
 }
